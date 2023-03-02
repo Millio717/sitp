e219b3ffcf7081899b96d8860b8b91611e02393a
Roman <95275689+Millio717@users.noreply.github.com>
2023-03-02 20:55:07 +0100

    str
    str

---
+++ js/index.js
@@ -0,0 +1,7 @@
+// const burger = document.querySelector('.burger');
+// const link = document.querySelector('.nav__links');
+
+// burger.addEventListener('click', function() {
+//     burger.classList.toggle('active');
+//     link.classList.toggle('active');
+// })
